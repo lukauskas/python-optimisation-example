46ef89f20679e86a346e37afad5b9e0d8d47ee11
Saulius Lukauskas <luksaulius@gmail.com>
2014-02-04 00:26:42 +0000

    added numpy bindings to setup.py

--- alignment/setup.py
+++ alignment/setup.py
@@ -1,8 +1,10 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+import numpy
 
 setup(
     cmdclass = {'build_ext': build_ext},
-    ext_modules = [Extension("align_cythonised", ["align.pyx"])]
+    ext_modules = [Extension("align_cythonised", ["align.pyx"],
+                   include_dirs=[numpy.get_include()])]
 )
